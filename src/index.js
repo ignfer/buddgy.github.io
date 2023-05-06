55c9f66a8dc9df8ad1b734f5040bd4d0f6f8bd9b
Ignacio Fernández <nachofernandezuru@gmail.com>
2023-05-06 09:55:31 -0300

    update
    la altura del main ahora se calcula con javascript

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 function to_github(){
     window.location.href = "https://github.com/ignfer";
 }
+
+function main_height(){
+    let altura = document.documentElement.clientHeight;
+    let altura2 = altura -= 60;
+    const main_element = document.getElementsByClassName('main')[0];
+
+    if(altura < 1500){
+        main_element.style.height = altura2 +'px';
+        console.log("nueva altura = " + altura2 + "/" + altura);
+    }
+}
