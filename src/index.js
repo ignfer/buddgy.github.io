bf3212082b615eddff709b20802df7715ee2bbd5
Ignacio Fernández <nachofernandezuru@gmail.com>
2023-05-12 10:22:54 -0300

    responsive
    se cambia el diseño de la pagina (ojala por ultima vez) y se logra un diseño responsive que finalmente funciona como se deseaba

--- src/index.js
+++ src/index.js
@@ -2,20 +2,6 @@ function to_github(){
     window.location.href = "https://github.com/ignfer";
 }
 
-function main_height(){
-    const max_height = 1500;
-    const altura_header = 60; //altura en px del header
-    let altura = document.documentElement.clientHeight; //altura inicial de los elementos
-    let new_altura = altura -= altura_header; //resultado de restarle el alto del header
-    const main_element = document.getElementsByClassName('main')[0]; //elemento 'main'
-    const main_area_element = document.getElementsByClassName('main_area')[0]; //elemento 'main_area'
-
-    if(altura < max_height){
-        main_element.style.height = new_altura +'px';
-        main_area_element.style.height = new_altura +'px';
-    }
-}
-
 function ingresar_gasto(){
     const titulo = document.getElementsByClassName('new_tittle_input')[0].value;
     const cuerpo = document.getElementsByClassName('new_descr_input')[0].value;
